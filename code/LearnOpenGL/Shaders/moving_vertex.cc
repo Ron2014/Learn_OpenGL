52fafe68fb0b46e3ca2b572fcd33d6480a89c92f
ron2014 <dev_ron@163.com>
2020-06-10 21:09:17 +0800

    new files

---
+++ code/LearnOpenGL/Shaders/moving_vertex.cc
@@ -0,0 +1,125 @@
+#include <iostream>
+using namespace std;
+
+#include <vector>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "shader.h"
+
+unsigned int WIN_WIDTH = 800;
+unsigned int WIN_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+  cout << "veiw change " << width << " " << height << endl;
+  glViewport(0, 0, width, height);
+
+  WIN_WIDTH = width;
+  WIN_HEIGHT = height;
+}  
+
+void processInput(GLFWwindow *window)
+{
+  if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);           // response to ESC
+  else if(glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+    if ((int)glfwGetTime()%2)
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    else
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+  }
+}
+
+int main(int argc, char *argv[]) {
+  glfwInit();
+  
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  // two lines tell the version of OpenGL is 3.3
+  // 用 Lab/Test/version.cc 工具查看OpenGL版本
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  // will use core-profile
+  // 我们只会用到OpenGL的子集，无需向后兼容的特性
+
+  // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);   // open in Mac OS X
+
+  GLFWwindow* window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, "Hello Triangle", NULL, NULL);
+  if (window == NULL)
+  {
+      cout << "Failed to create GLFW window" << endl;
+      glfwTerminate();
+      return -1;
+  }
+  glfwMakeContextCurrent(window);         // 当前线程与window绑定
+  glfwSetWindowSizeCallback(window, framebuffer_size_callback);
+
+  // glad 必须在 windows 创建成功后初始化
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    cout << "Failed to initialize GLAD" << endl;
+    return -1;
+  }
+
+  // shader source -> shader object -> shader program
+  Shader shader("E:\\GitHub\\Learn_OpenGL\\res\\shaders\\vertex_shader_moving.shader");
+
+  float vertices[] = {
+    // position           // color
+    -0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,     // left-bottom is red
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,     // right-bottom is green
+     0.0f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,     // top is blue
+  };
+
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // 顶点属性0 layout (location = 0)
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  // 顶点属性1 layout (location = 1) 注意起始位置要越过前面表示位置的3个分量float
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  // unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0); 
+
+  float beginTime = glfwGetTime();
+
+  // render loop:
+  while(!glfwWindowShouldClose(window))
+  {
+    // input
+    processInput(window);
+
+    // render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    float timeValue = glfwGetTime() - beginTime;
+    float offsetX = sin(timeValue) * 0.5f;        // !!! 秘技: 按正弦曲线左右横跳 !!!
+    shader.setFloat("x_offset", offsetX);
+
+    // draw our first triangle
+    shader.use();
+    glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwSwapBuffers(window);              // double buffer switch
+    glfwPollEvents();                     // keyboard/mouse event
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+
+  glfwTerminate();
+  return 0;
+}
